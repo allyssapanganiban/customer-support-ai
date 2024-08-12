be1a7079e066cf59e9f19d7e0970b50522caedc4
mafruhamanal <mafruhamanal1@gmail.com>
2024-08-11 23:39:51 -0400

    .

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Head from "next/head";
 import "./globals.css";
 import Chatbot from "./components/Chatbot";
 import { auth } from "./utils/firebaseConfig";
@@ -9,7 +10,9 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
   const [authenticated, setAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (!user) {
@@ -17,10 +20,26 @@ export default function Home() {
       } else {
         setAuthenticated(true);
       }
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, [router]);
 
-  return <Chatbot />;
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Education Bot</title>
+        <meta
+          name="description"
+          content="Effortlessly navigate your academic journey with our AI-driven customer support bot."
+        />
+      </Head>
+      {authenticated ? <Chatbot /> : null}
+    </>
+  );
 }
